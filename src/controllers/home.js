b256bcd21173106a68e0fcebfcec59740e2627ca
Terry Tice <nufyoot@gmail.com>
2013-05-27 23:27:32 -0400

    Using separators for stupid Azure

--- src/controllers/home.js
+++ src/controllers/home.js
@@ -1,8 +1,18 @@
-var KallyRazor = require('kally-razor');
-var razor = new KallyRazor({
-    root: __dirname + '/../views/home',
-    layout: __dirname + '/../views/shared/_layout.html'
-})
+var KallyRazor  = require('kally-razor');
+var path        = require('path');
+
+
+if (path.sep == '/') {
+    var razor = new KallyRazor({
+        root: __dirname + '/../views/home',
+        layout: __dirname + '/../views/shared/_layout.html'
+    });
+} else {
+    var razor = new KallyRazor({
+        root: __dirname + '\\..\\views\\home',
+        layout: __dirname + '\\..\\views\\shared\\_layout.html'
+    });
+}
 
 module.exports = {
     index: function (req, res) {
